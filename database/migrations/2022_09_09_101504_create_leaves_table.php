0a4b1ac08de63412e97697a6d285a09e26bf376c
chitkoko-dec17 <chitkoko.dec17@gmail.com>
2022-09-11 18:27:57 +0630

    update migration file

--- database/migrations/2022_09_09_101504_create_leaves_table.php
+++ database/migrations/2022_09_09_101504_create_leaves_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAttendancesTable extends Migration
+class CreateLeavesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -35,6 +35,6 @@ class CreateAttendancesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('attendances');
+        Schema::dropIfExists('leaves');
     }
 }
